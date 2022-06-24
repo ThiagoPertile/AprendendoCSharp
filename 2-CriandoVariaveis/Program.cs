ad7c883d68504f495c21bdab267ab02e54221458
ThiagoPertile <thiagomcarmo@outlook.com>
2022-06-24 17:53:09 -0300

    Criei Variaveis Idade

--- 2-CriandoVariaveis/Program.cs
+++ 2-CriandoVariaveis/Program.cs
@@ -1,2 +1,24 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 2 - Criando Variaveis");
+        int idade;
+
+        idade = 27;
+        Console.WriteLine(idade);
+        idade = idade - 1;
+        Console.WriteLine(idade);
+        idade = 5 * 2 - 6;
+        Console.WriteLine(idade);
+        idade = (5 - 2) * 3;
+        Console.WriteLine(idade);
+
+
+        Console.WriteLine("minha idade eh " + idade);
+        Console.WriteLine("tecle Enter para fechar");
+        Console.ReadLine();
+    }
+}
