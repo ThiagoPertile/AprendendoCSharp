64690b41829c9d4e18dae6339d31484d0752b1ef
ThiagoPertile <thiagomcarmo@outlook.com>
2022-06-27 18:03:06 -0300

    fiz progama 5

---
+++ 5-CaracteresETextos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Execultando o projeto 5 - Caracteres e Textos");
+        
+        
+        //no CHAR, diferentemente da STRING se usa aspas simples ('')
+        char letra = 'a';
+        Console.WriteLine(letra);
+        letra = (char)65;
+        Console.WriteLine(letra);
+        letra = (char)(65 + 1);
+        Console.WriteLine(letra);
+        letra = (char)(88 + 1);
+        Console.WriteLine(letra);
+
+
+        Console.WriteLine("Press ENTER para sair...");
+        Console.ReadLine();
+    }
+}
